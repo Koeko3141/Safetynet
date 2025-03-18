abda9dccc7c4be9b18dd03cdfeedc84ed4fd9b89
Koeko314 <konstantin.koefler@stud.fh-campuswien.ac.at>
2025-03-18 22:50:24 +0100

    Added help site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
-import {StartPageComponent} from "./start-page/start-page.component";
-import {SelfcheckComponent} from "./selfcheck/selfcheck.component";
-import {LearningQuizComponent} from "./learning-quiz/learning-quiz.component";
-import {InteractiveStoryComponent} from "./interactive-story/interactive-story.component";
+import { StartPageComponent } from "./start-page/start-page.component";
+import { SelfcheckComponent } from "./selfcheck/selfcheck.component";
+import { LearningQuizComponent } from "./learning-quiz/learning-quiz.component";
+import { InteractiveStoryComponent } from "./interactive-story/interactive-story.component";
 import { ExtraInfoComponent } from './extra-info/extra-info.component';
+import { HelpSiteComponent } from './help-site/help-site.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'start-page', pathMatch: 'full'},
-  {path: 'start-page', pathMatch: 'full', component: StartPageComponent},
-  {path: 'self-check', pathMatch: 'full', component: SelfcheckComponent},
-  {path: 'learning-quiz', pathMatch: 'full', component: LearningQuizComponent},
-  {path: 'interactive-story', pathMatch: 'full', component: InteractiveStoryComponent},
-  {path: 'extra-info', pathMatch:'full', component: ExtraInfoComponent}
+  { path: '', redirectTo: 'start-page', pathMatch: 'full' },
+  { path: 'start-page', pathMatch: 'full', component: StartPageComponent },
+  { path: 'self-check', pathMatch: 'full', component: SelfcheckComponent },
+  { path: 'learning-quiz', pathMatch: 'full', component: LearningQuizComponent },
+  { path: 'interactive-story', pathMatch: 'full', component: InteractiveStoryComponent },
+  { path: 'extra-info', pathMatch: 'full', component: ExtraInfoComponent },
+  { path: 'help-site', pathMatch: 'full', component: HelpSiteComponent }
 ];
