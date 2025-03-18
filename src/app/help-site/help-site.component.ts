c11ffc4a82bbea43d3d299d40e5eba5dbe54ed8b
Koeko314 <konstantin.koefler@stud.fh-campuswien.ac.at>
2025-03-19 00:48:09 +0100

    Help Site Form added

--- src/app/help-site/help-site.component.ts
+++ src/app/help-site/help-site.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule,NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-help-site',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './help-site.component.html',
   styleUrl: './help-site.component.css'
 })
-export class HelpSiteComponent {
 
+export class HelpSiteComponent {
+  onSubmit(form:NgForm){
+      console.log('Form Data: ', form.value)
+    }
 }
