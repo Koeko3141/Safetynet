57fcc904c4a56a8159ee2faab98130adc5914b1b
FelixKohlboeck <f.kohlboeck@students.htl-leonding.ac.at>
2025-03-18 20:55:48 +0100

    routes changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {StartPageComponent} from "./start-page/start-page.component";
+import {SelfcheckComponent} from "./selfcheck/selfcheck.component";
+import {LearningQuizComponent} from "./learning-quiz/learning-quiz.component";
+import {InteractiveStoryComponent} from "./interactive-story/interactive-story.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: StartPageComponent, pathMatch: 'full'},
+  {path: 'self-check', pathMatch: 'full', component: SelfcheckComponent},
+  {path: 'learning-quiz', pathMatch: 'full', component: LearningQuizComponent},
+  {path: 'interactive-story', pathMatch: 'full', component: InteractiveStoryComponent},
+];
