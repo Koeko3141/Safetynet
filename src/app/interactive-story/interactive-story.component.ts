52fb84f97eac95e6d2fa6a11f5db60c0e5e1448c
FelixKohlboeck <f.kohlboeck@students.htl-leonding.ac.at>
2025-03-19 09:27:29 +0100

    story layout

--- src/app/interactive-story/interactive-story.component.ts
+++ src/app/interactive-story/interactive-story.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import storyData from './story.json';
+import {NgForOf, NgIf} from "@angular/common";
+
+class Choice {
+  text: string = '';
+  targetPageNumber: number = 0;  // Standardwert 0
+}
+
+class Page {
+  pageNumber: number = 0;
+  title: string = '';
+  content: string = '';
+  choices: Choice[] = [];
+}
 
 @Component({
   selector: 'app-interactive-story',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './interactive-story.component.html',
   styleUrl: './interactive-story.component.css'
 })
 export class InteractiveStoryComponent {
 
+  story: Page[] = storyData; // ✅ JSON-Daten in die Story laden
+  currentPage = signal(0);  // Startseite
+
+  get page(): Page {
+    return this.story.find(p => p.pageNumber === this.currentPage()) ?? {
+      pageNumber: -1, title: "Lade Story...", content: "", choices: []
+    };
+  }
+
+
+  choose(choice: Choice) {
+    this.currentPage.set(choice.targetPageNumber);
+  }
+
+  restartStory() {
+    this.currentPage.set(0);
+  }
 }
